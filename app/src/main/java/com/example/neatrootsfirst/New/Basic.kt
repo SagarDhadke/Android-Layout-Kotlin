a752e0551df76e7effd4523d0c4f54c75118d5dd
Sagar Dhadke <peaceunique2003@gmail.com>
2023-02-05 20:20:36 +0530

    Code Update

---
+++ app/src/main/java/com/example/neatrootsfirst/New/Basic.kt
@@ -0,0 +1,12 @@
+package com.example.neatrootsfirst.New
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.neatrootsfirst.R
+
+class Basic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic)
+    }
+}
---
+++ app/src/main/java/com/example/neatrootsfirst/constraintlayout/FoodOrder.kt
@@ -0,0 +1,56 @@
+package com.example.neatrootsfirst.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
+import com.example.neatrootsfirst.R
+
+//    var btnGroup : RadioGroup? = null
+//    lateinit var radioButton: RadioButton
+//
+//    //Textview
+//    lateinit var regular : TextView
+//    lateinit var small : TextView
+//    lateinit var large : TextView
+//
+//    //checkbox
+//    lateinit var check1 : CheckBox
+//    lateinit var check2 : CheckBox
+//    lateinit var check3 : CheckBox
+//    lateinit var check4 : CheckBox
+//
+//    //Total , subTotal , Ship
+//
+//    lateinit var total : TextView
+//    lateinit var subTotal : TextView
+//    lateinit var Ship : TextView
+//
+//    //Button
+//    lateinit var getBtn : Button
+
+
+class FoodOrder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_order)
+
+//        getBtn.findViewById<Button>(R.id.button)
+//
+//        btnGroup?.findViewById<RadioGroup>(R.id.radioGbtn)
+//
+//        getBtn.setOnClickListener {
+//
+//            val SelectRadio = btnGroup!!.checkedRadioButtonId
+//            radioButton = findViewById(SelectRadio)
+//
+//            Toast.makeText(applicationContext, radioButton.text,Toast.LENGTH_LONG).show()
+//
+//        }
+
+    }
+}
